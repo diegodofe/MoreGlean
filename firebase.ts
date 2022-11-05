c1d52b6a0e5ecb772959f7a0443f74c335af22bc
William Zijie Zhang <89562186+Transurgeon@users.noreply.github.com>
2022-11-05 16:57:56 -0400

    feature/implement-google-OA (#18)
    * fixing unused variables issue
    
    * Disable button for now
    
    Co-authored-by: Trevor Hegarty <tomheg97@protonmail.com>

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,3 +22,4 @@ initializeApp(firebaseConfig)
 const db = getFirestore()
 
 export default db
+export const auth = getAuth()
