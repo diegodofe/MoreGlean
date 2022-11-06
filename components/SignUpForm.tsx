d4359c95cfe36d9afad9565bc7a5a595ab3356d2
Huining Liu <huiningliu@Huinings-MacBook-Pro.local>
2022-11-05 20:09:37 -0400

    Changes in Sign Up form

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -1,37 +1,33 @@
-/* eslint-disable react/jsx-props-no-spreading */
-
-// const formItemLayout = {
-//   labelCol: {
-//     xs: { span: 30 },
-//     sm: { span: 8 },
-//   },
-//   wrapperCol: {
-//     xs: { span: 30 },
-//     sm: { span: 16 },
-//   },
-// }
-
-// const tailFormItemLayout = {
-//   wrapperCol: {
-//     xs: {
-//       span: 24,
-//       offset: 0,
-//     },
-//     sm: {
-//       span: 16,
-//       offset: 0,
-//     },
-//   },
-// }
+import { Button, Checkbox, Col, Form, Input, Radio, Row } from 'antd'
 
 export default function SignUpForm() {
   return (
     <div>
-      {/* <Row>
+      <Row>
         <Col span={24}>
-          <Form {...formItemLayout} layout='vertical'>
+          <Form
+            labelCol={{
+              xs: { span: 24 },
+              sm: { span: 40 },
+            }}
+            wrapperCol={{
+              xs: { span: 24 },
+              sm: { span: 30 },
+            }}
+            layout='vertical'
+          >
             <h1>Complete Your Profile</h1>
-            <Form.Item id='role' name='role' label='What is your role?'>
+            <Form.Item
+              id='role'
+              name='role'
+              label='What is your role?'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please choose your role!',
+                },
+              ]}
+            >
               <Radio.Group>
                 <Radio value='farm'> Farm </Radio>
                 <Radio value='user'> User </Radio>
@@ -49,9 +45,9 @@ export default function SignUpForm() {
             >
               <Input />
             </Form.Item>
-            <Form.Item label='Address'>
+            {/* <Form.Item label='Address'>
               <Input />
-            </Form.Item>
+            </Form.Item> */}
             <Form.Item
               name='email'
               label='E-mail'
@@ -69,134 +65,6 @@ export default function SignUpForm() {
               <Input />
             </Form.Item>
             <Form.Item
-              name='password'
-              label='Password'
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your password!',
-                },
-              ]}
-              hasFeedback
-            >
-              <Input.Password />
-            </Form.Item>
-            <Form.Item
-              name='confirm'
-              label='Confirm Password'
-              dependencies={['password']}
-              hasFeedback
-              rules={[
-                {
-                  required: true,
-                  message: 'Please confirm your password!',
-                },
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    if (!value || getFieldValue('password') === value) {
-                      return Promise.resolve()
-                    }
-                    return Promise.reject(
-                      new Error(
-                        'The two passwords that you entered do not match!'
-                      )
-                    )
-                  },
-                }),
-              ]}
-            >
-              <Input.Password />
-            </Form.Item>
-            <Form.Item
-              name='groupsize'
-              dependencies={['role']}
-              hasFeedback
-              label='Number of people in your group.'
-              rules={[
-                {
-                  required: true,
-                  message: 'Please enter the number of people in your group.',
-                },
-                // eslint-disable-next-line no-empty-pattern
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    if (
-                      value != null &&
-                      value !== '' &&
-                      !Number.isNaN(Number(value.toString()))
-                    ) {
-                      let foodName = (
-                        document.getElementById('role') as HTMLInputElement
-                      ).value
-                      console.log(foodName)
-                      return Promise.resolve()
-                    }
-                    return Promise.reject(
-                      new Error('Please enter a valid number.')
-                    )
-                  },
-                }),
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name='capacity'
-              label='Food Capacity in Kg'
-              rules={[
-                {
-                  required: true,
-                  message: 'Please enter the food capacity.',
-                },
-                // eslint-disable-next-line no-empty-pattern
-                ({}) => ({
-                  validator(_, value) {
-                    if (
-                      value != null &&
-                      value !== '' &&
-                      !Number.isNaN(Number(value.toString()))
-                    ) {
-                      return Promise.resolve()
-                    }
-                    return Promise.reject(
-                      new Error('Please enter a valid number.')
-                    )
-                  },
-                }),
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name='distance'
-              label='Distance in Km'
-              rules={[
-                {
-                  required: true,
-                  message:
-                    'Please enter the distance you are willing to travel.',
-                },
-                // eslint-disable-next-line no-empty-pattern
-                ({}) => ({
-                  validator(_, value) {
-                    if (
-                      value != null &&
-                      value !== '' &&
-                      !Number.isNaN(Number(value.toString()))
-                    ) {
-                      return Promise.resolve()
-                    }
-                    return Promise.reject(
-                      new Error('Please enter a valid number.')
-                    )
-                  },
-                }),
-              ]}
-            >
-              <Input defaultValue={0} min={0} />
-            </Form.Item>
-
-            <Form.Item
               name='agreement'
               valuePropName='checked'
               rules={[
@@ -207,7 +75,6 @@ export default function SignUpForm() {
                       : Promise.reject(new Error('Should accept agreement')),
                 },
               ]}
-              {...tailFormItemLayout}
             >
               <Checkbox>
                 By clicking on sign-up, you agree to MoreGlean’s Terms and
@@ -221,7 +88,7 @@ export default function SignUpForm() {
             </Form.Item>
           </Form>
         </Col>
-      </Row> */}
+      </Row>
     </div>
   )
 }
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -3,7 +3,6 @@ import SignUpForm from '../../components/SignUpForm'
 export default function SignUp() {
   return (
     <div>
-      <h1>Sign up</h1>
       <SignUpForm />
     </div>
   )
