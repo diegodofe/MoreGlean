4f534f9545aeae5e056035bceeb647b085d77a33
Diego Dorantes-Ferreira <63019625+diegodofe@users.noreply.github.com>
2022-11-07 07:31:01 -0500

    feature/enhance-left-nav (#95)
    * Finish left nav
    
    * Fix nav bar

--- components/CreateEventTab.tsx
+++ components/CreateEventTab.tsx
@@ -5,7 +5,10 @@ import {
   FilePicker,
   Heading,
   Pane,
+  PlusIcon,
   SelectMenu,
+  Tab,
+  Text,
   TextInputField,
   toaster,
 } from 'evergreen-ui'
@@ -18,7 +21,7 @@ import { createEvent, getEventByDocRef } from '../services/event'
 import { DateValue } from '../types/dates'
 import { EventData } from '../types/events'
 
-export default function CreateEventButton() {
+export default function CreateEventTab() {
   const [isCreateEventShown, setIsCreateEventShown] = useState(false)
   const [eventTitle, setEventTitle] = useState('')
   const [foodbank, setFoodbank] = useState('')
@@ -78,7 +81,16 @@ export default function CreateEventButton() {
 
   return (
     <>
-      <Button onClick={() => setIsCreateEventShown(true)}>Create Event</Button>
+      <Tab
+        id='Create-Event'
+        direction='vertical'
+        aria-controls='panel-Group'
+        onSelect={() => setIsCreateEventShown(true)}
+      >
+        <Pane display='flex' alignItems='center' gap={8}>
+          <PlusIcon size={24} /> <Text>Create Event</Text>
+        </Pane>
+      </Tab>
       <Dialog
         isShown={isCreateEventShown}
         title='Create an event'
--- pages/groups/index.tsx
+++ pages/groups/index.tsx
@@ -1,10 +1,5 @@
 import { Pane } from 'evergreen-ui'
-import CreateGroupButton from '../../components/CreateGroupButton'
 
 export default function Group() {
-  return (
-    <Pane>
-      <CreateGroupButton />
-    </Pane>
-  )
+  return <Pane>groups</Pane>
 }
