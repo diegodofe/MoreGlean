fa29c708c87cdb1c42bb6cbf9a929ad3571a9154
saurabh845 <SaurabhJain666@gmail.com>
2022-11-06 19:06:53 -0500

    Added Google Maps frame (#51)
    Co-authored-by: Saurabh Jain <saurabhjain@Saurabhs-MacBook-Air.local>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { Button, Heading, Pane } from 'evergreen-ui'
+import { GeoPoint } from 'firebase/firestore'
 import Head from 'next/head'
 import Link from 'next/link'
+import LocationMap from '../components/LocationMap'
 
 export default function Home() {
   return (
@@ -23,6 +25,8 @@ export default function Home() {
           <Link href='/users'>
             <Button appearance='link'>View all users</Button>
           </Link>
+
+          <LocationMap location={new GeoPoint(45.5019, -73.5674)} />
         </Pane>
       </Pane>
     </>
